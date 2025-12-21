4e13fd00eb7a122166098f18a620c89a545cf68b
Eric <eribo2@web.de>
2025-12-21 15:26:03 +0100

    added methodes to generate word to guess by using the frequenzy of letters in the remaining words.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 class WordleSolver:
     def __init__(self):
-        self.word_list = []
+        self.__word_list = []
+        self.__letter_total_dict = {}
+        self.__letter_freq_dict = {}
 
 
     def create_word_list(self):
@@ -10,29 +12,71 @@ class WordleSolver:
         for words in f.readlines():
             list_words.append(words.strip())
         f.close()
-        self.word_list = list_words
+        self.__word_list = list_words
 
     def get_word_list(self):
-        return self.word_list
+        return self.__word_list
 
     def delete_word(self, letter):
-        self.word_list = [word for word in self.word_list if letter not in word]
+        self.__word_list = [word for word in self.__word_list if letter not in word]
 
     def in_word(self, letter):
-        self.word_list = [word for word in self.word_list if letter in word]
+        self.__word_list = [word for word in self.__word_list if letter in word]
 
     def at_pos(self, letter, pos):
-        self.word_list = [word for word in self.word_list if letter is word[pos]]
+        self.__word_list = [word for word in self.__word_list if letter is word[pos-1]]
 
     def not_at_pos(self, letter, pos):
         self.in_word(letter)
-        self.word_list = [word for word in self.word_list if letter is not word[pos]]
+        self.__word_list = [word for word in self.__word_list if letter is not word[pos-1]]
+
+    def create_letter_freq(self):
+        for word in self.__word_list:
+            for i in word:
+                x = self.__letter_total_dict.get(i)
+                if x is None:
+                    self.__letter_total_dict.update({i: 1})
+                else:
+                    self.__letter_total_dict.update({i: x + 1})
+
+        total_letters = len(self.__word_list) * 5
+
+        for key in self.__letter_total_dict:
+            self.__letter_freq_dict[key] = (self.__letter_total_dict[key]/total_letters)
+
+    def get_letter_freq(self):
+        return self.__letter_freq_dict
+
+    def get_letter_total(self):
+        return self.__letter_total_dict
+
+    def get_word_rec(self):
+        best_word = ""
+        best_value = 0
+        for word in self.__word_list:
+            current_value = 0
+            current_word = word
+            used_letters = []
+            for letter in current_word:
+                if letter not in used_letters:
+                    used_letters.append(letter)
+                    current_value += self.__letter_freq_dict[letter]
+
+            if current_value > best_value:
+                best_value = current_value
+                best_word = current_word
+        return best_word
 
 solver = WordleSolver()
 solver.create_word_list()
 
-solver.at_pos("a", 0)
-solver.at_pos("e", 4)
-solver.not_at_pos("d", 2)
-print(solver.get_word_list())
+solver.delete_word("e")
+solver.delete_word("r")
+solver.delete_word("o")
+solver.not_at_pos("a", 1)
+solver.not_at_pos("s", 5)
+
+solver.create_letter_freq()
+
+print(solver.get_word_rec())
 
